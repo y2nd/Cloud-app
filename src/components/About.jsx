393a21b082d8fc09c8a7747bc35f75e1628ef67a
Youssef T <sheese.711@gmail.com>
2022-06-20 16:58:47 +0200

    started About section

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <div className="">
+            <div className="">
+                <h2>Trusted by developers across the world</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et porro vel ipsum voluptatem est! Quisquam fuga architecto vitae.</p>
+            </div>
+
+            <div className="">
+                <div className="">
+                    <p>100%</p>
+                    <p>Completion</p>
+                </div>
+
+                <div className="">
+                    <p>24/7</p>
+                    <p>Delivery</p>
+                </div>
+
+                <div className="">
+                    <p>100K</p>
+                    <p>Transactions</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
